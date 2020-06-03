958c7fcd6893cf04d85984e7b8e2c425cc27fda5
Tovly Deutsch <tovlydeutsch@gmail.com>
2020-06-03 00:46:27 -0400

    changed blog link to own site

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import tw from 'tailwind.macro'
 import React from 'react'
 import styled from 'styled-components'
+import { Link } from 'gatsby'
 import { OutboundLink } from 'gatsby-plugin-google-analytics'
 import PropTypes from 'prop-types'
 import Img from 'gatsby-image'
@@ -18,19 +19,22 @@ const Social = styled.section`
   ${tw`flex flex-wrap items-center justify-center xl:justify-start my-8`};
 `
 
-const Button = styled(OutboundLink)`
+const genButton = component => styled(component)`
   ${tw`cursor-pointer text-sm md:text-base mx-2 sm:mx-0 py-2 px-4 md:px-8 rounded-full no-underline shadow-md focus:outline-none focus:shadow-outline`};
   transition: all 0.3s ease-in-out;
   &:hover {
     transform: translateY(-1px);
   }
 `
+const OutboundButton = genButton(OutboundLink)
+const Button = genButton(Link)
 
-const GitHub = styled(Button)`
+// TODO see if I can merge these into one button with flexbox
+const GitHub = styled(OutboundButton)`
   ${tw`bg-blue-dark text-white sm:ml-0 sm:mr-4 my-1`};
 `
 
-const Twitter = styled(Button)`
+const LastButton = styled(Button)`
   ${tw`bg-blue-dark text-white`};
 `
 
@@ -47,7 +51,7 @@ const Header = ({ faceFile }) => (
             <OutboundLink href="http://www.eecs.harvard.edu/shieber/"> Stuart Shieber</OutboundLink> and
             <OutboundLink href="https://jasbi.github.io/"> Masoud Jasbi</OutboundLink>. In my spare time, I enjoy
             <OutboundLink href="https://www.youtube.com/channel/UCoYCQPyiv_y4QAHinPOFGHA"> filmmaking</OutboundLink> and
-            <OutboundLink href="https://medium.com/@tovly"> blogging</OutboundLink>.
+            <Link to="/blog"> blogging</Link>.
           </p>
         </Description>
         <Social>
@@ -63,9 +67,9 @@ const Header = ({ faceFile }) => (
           <GitHub role="button" href="/TovlyDeutschResume.pdf">
             Resume
           </GitHub>
-          <Twitter role="button" href="https://medium.com/@tovly">
+          <LastButton role="button" to="/blog">
             Blog
-          </Twitter>
+          </LastButton>
         </Social>
       </div>
       <Img fixed={faceFile.fixed} style={{ margin: 'auto', marginTop: 24 }} />
